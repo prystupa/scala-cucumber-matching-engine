3a479ac1e77307a316c69e3397cfbcb6cedc010e
Eugene Prystupa <prystupa@sqlapi.com>
2013-01-05 22:47:54 -0500

    - refactoring: introducing OrderBookEvent and making MatchingEngine publish trades as they happen as apposed to returning a list

---
+++ post-2-crossing-limit-orders/src/main/scala/com/prystupa/matching/OrderBookEvent.scala
@@ -0,0 +1,13 @@
+package com.prystupa.matching
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eprystupa
+ * Date: 12/22/12
+ * Time: 11:03 PM
+ */
+
+sealed trait OrderBookEvent
+
+case class Trade(buyingBroker: String, sellingBroker: String,
+                 price: Double, qty: Double) extends OrderBookEvent
