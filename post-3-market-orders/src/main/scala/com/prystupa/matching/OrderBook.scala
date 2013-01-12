c86038c62cd4c4c199243f5a4bf8aeac2d8cfe3a
Eugene Prystupa <prystupa@sqlapi.com>
2013-01-12 15:28:52 -0500

    - refactoring post-3 code

--- post-3-market-orders/src/main/scala/com/prystupa/matching/OrderBook.scala
+++ post-3-market-orders/src/main/scala/com/prystupa/matching/OrderBook.scala
@@ -11,7 +11,7 @@ package com.prystupa.matching
 class OrderBook(side: Side, orderTypes: (Order => OrderType)) {
 
   private var market: List[Order] = Nil
-  private var limit: List[(Double, List[Order])] = Nil
+  private var limitBook: List[(Double, List[Order])] = Nil
   private val priceOrdering = if (side == Sell) Ordering[Double] else Ordering[Double].reverse
 
   def add(order: Order) {
@@ -19,24 +19,13 @@ class OrderBook(side: Side, orderTypes: (Order => OrderType)) {
     orderTypes(order).price match {
 
       case MarketPrice => market = market :+ order
-
-      case LimitPrice(level) => {
-        def insert(list: List[(Double, List[Order])]): List[(Double, List[Order])] = list match {
-          case Nil => List((level, List(order)))
-          case (head@(bookLevel, orders)) :: tail => priceOrdering.compare(level, bookLevel) match {
-            case 0 => (bookLevel, orders :+ order) :: tail
-            case n if n < 0 => (level, List(order)) :: list
-            case _ => head :: insert(tail)
-          }
-        }
-        limit = insert(limit)
-      }
+      case LimitPrice(limit) => addLimit(limit, order)
     }
   }
 
   def top: Option[Order] = market match {
     case head :: _ => Some(head)
-    case _ => limit.headOption.map({
+    case _ => limitBook.headOption.map({
       case (_, orders) => orders.head
     })
   }
@@ -45,10 +34,10 @@ class OrderBook(side: Side, orderTypes: (Order => OrderType)) {
 
     market match {
       case top :: tail => market = if (qty == top.qty) tail else orderTypes(top).decreasedBy(qty) :: tail
-      case _ => limit match {
+      case _ => limitBook match {
         case ((level, orders) :: tail) => {
           val (top :: rest) = orders
-          limit = (qty == top.qty, rest.isEmpty) match {
+          limitBook = (qty == top.qty, rest.isEmpty) match {
             case (true, true) => tail
             case (true, false) => (level, rest) :: tail
             case _ => (level, orderTypes(top).decreasedBy(qty) :: rest) :: tail
@@ -59,7 +48,20 @@ class OrderBook(side: Side, orderTypes: (Order => OrderType)) {
     }
   }
 
-  def orders(): List[Order] = market ::: limit.flatMap({
+  def orders(): List[Order] = market ::: limitBook.flatMap({
     case (_, orders) => orders
   })
+
+  private def addLimit(limit: Double, order: Order) {
+    def insert(list: List[(Double, List[Order])]): List[(Double, List[Order])] = list match {
+      case Nil => List((limit, List(order)))
+      case (head@(bookLevel, orders)) :: tail => priceOrdering.compare(limit, bookLevel) match {
+        case 0 => (bookLevel, orders :+ order) :: tail
+        case n if n < 0 => (limit, List(order)) :: list
+        case _ => head :: insert(tail)
+      }
+    }
+
+    limitBook = insert(limitBook)
+  }
 }
